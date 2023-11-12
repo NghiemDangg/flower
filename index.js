5681bbacd798f49a058c2b3ed837323f5d0469dd
nghiemDang <>
2023-11-13 00:20:33 +0700

    comment logic schedule

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const scheduledTime = new Date("November 12, 2023 23:59:00").getTime();
-  const intervalId = setInterval(function () {
-    const currentTime = new Date().getTime();
-    const linkElement = document.getElementById("route-link");
-    const textContent = document.getElementById("text-content");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const scheduledTime = new Date("November 12, 2023 23:59:00").getTime();
+//   const intervalId = setInterval(function () {
+//     const currentTime = new Date().getTime();
+//     const linkElement = document.getElementById("route-link");
+//     const textContent = document.getElementById("text-content");
 
-    if (currentTime >= scheduledTime) {
-      console.log(linkElement);
-      console.log(textContent);
-      textContent.innerHTML = "Bấm để nhận hoa!";
-      linkElement.href = "flower.html";
-      clearInterval(intervalId);
-    }
-  }, 1000); // Kiểm tra mỗi giây (1000 milliseconds)
-});
+//     if (currentTime >= scheduledTime) {
+//       console.log(linkElement);
+//       console.log(textContent);
+//       textContent.innerHTML = "Bấm để nhận hoa!";
+//       linkElement.href = "flower.html";
+//       clearInterval(intervalId);
+//     }
+//   }, 1000); // Kiểm tra mỗi giây (1000 milliseconds)
+// });
